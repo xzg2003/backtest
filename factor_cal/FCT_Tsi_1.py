108b55268adaa7bd9d756e08abac9a359a783ba1
IVANLEE <lee162286@gmail.com>
2025-08-19 16:33:30 +0800

    refactor(factor_cal): 重构 TSI 指标计算逻辑- 移除了未使用的 numpy导入 - 替换了变量名称，使代码更易理解- 优化了 EMA 计算过程，减少了重复代码 - 调整了 TSI 的计算公式，去除了不必要的常数

--- factor_cal/FCT_Tsi_1.py
+++ factor_cal/FCT_Tsi_1.py
@@ -2,7 +2,6 @@
 
 import os
 
-import numpy
 import pandas
 
 # 设置工作目录为当前脚本所在的目录
@@ -19,11 +18,8 @@ class FCT_Tsi_1:
         if df is None:
             raise ValueError("no 'df' in param")
 
-        # 从参数字典中提取 short, long
-        short = param.get('short', None)
-        long = param.get('long', None)
-        if short or long is None:
-            raise ValueError("param missing 'short' or 'long'")
+        # 从参数字典中提取 length
+        length = param.get('length', None)
 
         # 从参数字典中提取 factor_name
         factor_name = param.get('factor_name', None)
@@ -33,21 +29,17 @@ class FCT_Tsi_1:
         # 初始化 new_columns 用于集中管理中间变量
         new_columns = pandas.DataFrame(index=df.index)
 
-        # 计算价格变化
-        new_columns['diff'] = df['close'].diff()
+        new_columns['price_change'] = df['close'].diff()  # 计算价格变化
+        new_columns['pc_ema1'] = new_columns['price_change'].ewm(span=length).mean()  # 第一次平滑
+        new_columns['pc_ema2'] = new_columns['pc_ema1'].ewm(span=length).mean()  # 第二次平滑
 
-        # 计算两次 EMA
-        ema_1 = new_columns['diff'].ewm(span=short, adjust=False).mean()
-        ema_2 = ema_1.ewm(span=short, adjust=False).mean()
-
-        # 计算绝对值部分的两次 EMA
-        abs_diff = numpy.abs(new_columns['diff'])
-        abs_ema_1 = abs_diff.ewm(span=short, adjust=False).mean()
-        abs_ema_2 = abs_ema_1.ewm(span=long, adjust=False).mean()
+        # 对价格变化绝对值进行相同处理
+        new_columns['abs_pc'] = new_columns['price_change'].abs()
+        new_columns['abs_pc_ema1'] = new_columns['abs_pc'].ewm(span=length).mean()
+        new_columns['abs_pc_ema2'] = new_columns['abs_pc_ema1'].ewm(span=length).mean()
 
         # 计算 TSI
-        new_columns[f'{factor_name}'] = 100 * (ema_2 / (abs_ema_2 + 1e-10))
-
+        new_columns[f'{factor_name}'] = (new_columns['pc_ema2'] / new_columns['abs_pc_ema2'])
         # 合并到原始 df
         df = pandas.concat([df, new_columns], axis=1)
 
